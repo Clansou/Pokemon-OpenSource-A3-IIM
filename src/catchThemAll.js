dacc084522c8feeb3c34f623ce1f1cc9a44149cd
Clansou <clement.souplet78@gmail.com>
2024-01-09 12:08:04 +0100

    :memo: Add return tags

--- src/catchThemAll.js
+++ src/catchThemAll.js
@@ -1,7 +1,13 @@
-export default async function catchThemAll(pokemonid) {
-    console.log(pokemonid);
+/**
+ * Get a pokemon and check if it's caught.
+ * @param {number|string} pokemon - The ID or name of the pokemon to be fetched and checked for catchability.
+ * @returns {boolean} - A boolean indicating whether the pokemon can be caught or not.
+ * @throws {Error} - Throws an error if there is an issue fetching the pokemon data.
+ */
+export default async function catchThemAll(pokemon) {
+    console.log(pokemon);
     try {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonid}`);
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemon}`);
         const data = await response.json();
         const tryToCatch = Math.floor(Math.random() * 255 + 1);
         return tryToCatch <= data["capture_rate"] ? true : false;
