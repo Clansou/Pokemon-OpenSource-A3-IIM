1649c8cbbf418e4c2f00c46da5326419f157aba0
Clansou <clement.souplet78@gmail.com>
2024-01-09 14:09:49 +0100

    Modify catchThemAll test

--- test/catchThemAll.test.js
+++ test/catchThemAll.test.js
@@ -3,9 +3,9 @@ import getAleatoirePokemon from '../src/getPokemon';
 
 describe('catchThemAll(data)', () => {
     it('should return data of pokemon', async () => {
-            const pokemonid = await getAleatoirePokemon();
-            const result = await catchThemAll(pokemonid["id"]);
-            console.log(result);
-            expect(typeof result).toBe('boolean');
-      });
+        const pokemonid = await getAleatoirePokemon();
+        const result = await catchThemAll(pokemonid["id"]);
+        console.log(result);
+        expect(typeof result).toBe('boolean');
+    });
   });
